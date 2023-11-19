52e147997108f7e6770acec2b961327cf99d87c3
Adam Bouqdib <adam@abemedia.co.uk>
2023-11-19 17:31:02 +0000

    chore(apt): strong-typed checksums

--- integrations/apt/types.go
+++ integrations/apt/types.go
@@ -1,6 +1,14 @@
 package apt
 
-import "time"
+import (
+	"bufio"
+	"bytes"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"strconv"
+	"time"
+)
 
 type Release struct {
 	Origin       string
@@ -21,10 +29,68 @@ type Releases struct {
 	Architectures string
 	Components    string
 	Description   string
-	MD5Sum        string
-	SHA1          string
-	SHA256        string
-	SHA512        string
+	MD5Sum        Checksums[[16]byte]
+	SHA1          Checksums[[20]byte]
+	SHA256        Checksums[[32]byte]
+	SHA512        Checksums[[64]byte]
+}
+
+type sum interface {
+	[16]byte | [20]byte | [32]byte | [64]byte
+}
+
+type Checksums[T sum] []ChecksumFile[T]
+
+func (c Checksums[T]) MarshalText() ([]byte, error) {
+	if len(c) == 0 {
+		return nil, nil
+	}
+
+	b := make([]byte, 0, 512)
+	for _, info := range c {
+		b = append(b, fmt.Sprintf("\n%x %d %s", info.Sum, info.Size, info.Name)...)
+	}
+
+	return b, nil
+}
+
+var space = []byte{' '} //nolint:gochecknoglobals
+
+func (c *Checksums[T]) UnmarshalText(b []byte) error {
+	r := bufio.NewScanner(bytes.NewReader(b))
+	if !r.Scan() {
+		return nil
+	}
+
+	for r.Scan() {
+		a := bytes.SplitN(r.Bytes(), space, 3)
+		size, err := strconv.Atoi(string(a[1]))
+		if err != nil {
+			return err
+		}
+
+		var sum T
+		if hex.DecodedLen(len(a[0])) != len(sum) {
+			return errors.New("hex data would overflow byte array")
+		}
+		b := make([]byte, len(sum))
+		if _, err = hex.Decode(b, a[0]); err != nil {
+			return err
+		}
+		for i := 0; i < len(sum); i++ {
+			sum[i] = b[i]
+		}
+
+		*c = append(*c, ChecksumFile[T]{sum, size, string(a[2])})
+	}
+
+	return r.Err()
+}
+
+type ChecksumFile[T sum] struct {
+	Sum  T
+	Size int
+	Name string
 }
 
 type Package struct {
